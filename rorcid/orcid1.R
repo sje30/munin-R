4e827fe595ee4e883934701c87393f5d3ee707fe
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-08 21:40:12 +0000

    interactive usage

---
+++ rorcid/orcid1.R
@@ -0,0 +1,22 @@
+## Interactive usage of rorcid package
+
+## Install first from CRAN, e.g.
+## install.packages(c("rorcid", "rcrossref"))
+require(rorcid)
+require(rcrossref)
+
+## orcid for Stephen
+me <- "0000-0001-8607-8025"
+
+## convert to an ORCID!
+as.orcid(me)
+## Open a web browser of the orcid.
+browse(as.orcid(me))
+
+## Let's try to find Daniel.
+orcid(query = "Daniel Nüst geoinformatics")
+
+## bibtex file may take a minute or two to create.
+my_dois <- rorcid::identifiers(rorcid::works("0000-0002-0337-5997"))
+pubs <- rcrossref::cr_cn(dois = my_dois, format = "bibtex")
+invisible(lapply(pubs, write, "pubs.bib", append=TRUE))
