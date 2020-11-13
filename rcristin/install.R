717940859d77c1f71b88cd3d667a71171c6e86ed
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-13 11:05:14 +0000

    dependencies

--- rcristin/install.R
+++ rcristin/install.R
@@ -1,4 +1,8 @@
 require(remotes) || install.package("remotes")
+require(knitr) || install.package("knitr")
+require(dplyr) || install.package("dplyr")
+require(ggplot2) || install.package("ggplot2")
+
 remotes::install_github("henrikkarlstrom/rcristin")
 
 
