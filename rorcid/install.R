28e243c755db242ffadd64f3c59a650f5a7809b9
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-13 10:56:04 +0000

    document change

--- rorcid/install.R
+++ rorcid/install.R
@@ -1,6 +1,9 @@
 install.packages("DT")
 install.packages("rorcid")
 install.packages("remotes")
-remotes::install_github("ropensci/RefManageR")
 install.packages("xtable")
 install.packages("knitr")
+## As of November 2020, RefManageR is not available on CRAN due
+## to a glitch, so we need to load it from elsewhere.  We have edited
+## the examples such that RefManageR is now an optional dependency.
+remotes::install_github("ropensci/RefManageR")
