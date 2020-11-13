f01073b410702c693cf82693ec852de1ebd6001b
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-13 11:05:33 +0000

    whitespace for the win

--- rcristin/install.R
+++ rcristin/install.R
@@ -1,6 +1,6 @@
 require(remotes) || install.package("remotes")
-require(knitr) || install.package("knitr")
-require(dplyr) || install.package("dplyr")
+require(knitr)   || install.package("knitr")
+require(dplyr)   || install.package("dplyr")
 require(ggplot2) || install.package("ggplot2")
 
 remotes::install_github("henrikkarlstrom/rcristin")
