f38538f6729d30384061d3821c231c6cf0bbe10a
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-16 22:52:34 +0000

    why does ggplot2 complain?

--- .binder/install.R
+++ .binder/install.R
@@ -2,7 +2,7 @@ install.packages("devtools")
 require(remotes) || install.packages("remotes")
 require(knitr)   || install.packages("knitr")
 require(dplyr)   || install.packages("dplyr")
-require(ggplot2) || install.packages("ggplot2")
+install.packages("ggplot2")
 require(forcats) || install.packages("forcats")
 
 remotes::install_github("henrikkarlstrom/rcristin")
