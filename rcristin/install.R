eb760d174d6ce8fe5777ce2dff90084a0e33c910
Stephen J. Eglen <sje30@cam.ac.uk>
2020-11-16 21:46:58 +0000

    need forcats package too

--- rcristin/install.R
+++ rcristin/install.R
@@ -2,6 +2,7 @@ require(remotes) || install.package("remotes")
 require(knitr)   || install.package("knitr")
 require(dplyr)   || install.package("dplyr")
 require(ggplot2) || install.package("ggplot2")
+require(forcats) || install.package("forcats")
 
 remotes::install_github("henrikkarlstrom/rcristin")
 
